16307163f28d742d26395e111ec023ce35e027a0
akshitgarg4 <akshit2000garg@gmail.com>
2020-07-12 18:41:23 +0530

    Remove friend successful

--- controllers/api/friends.js
+++ controllers/api/friends.js
@@ -24,3 +24,36 @@ module.exports.create =async function(req,res)
         });
     
 }
+
+
+
+//to remove friendship
+module.exports.remove =async function(req,res)
+{
+    let user=await User.findOne({_id:req.query.user_id});
+    let user1=await User.findOne({_id:req.user._id});
+    if(!user || !user1 )
+    {
+        
+    }
+    else{
+        var newFriends=[]
+        let status = await user1.friends.forEach(element => {
+            if(element != req.query.user_id )
+            {
+                newFriends.push(element);
+            }
+            
+        });
+         user1.friends = newFriends;
+        let updated = await user1.save();
+    }
+    
+        let finalList= await User.findOne({_id:req.user._id}).populate('friends',"_id email name");
+        return res.json(200,{
+            message:"Friend removed",
+            success:true,
+            data:finalList
+        });
+    
+}
--- routes/api/v1/friends.js
+++ routes/api/v1/friends.js
@@ -6,6 +6,8 @@ const passport = require("passport");
 
 //post because it will take along the form inputs
 router.get('/create_friendship',passport.authenticate('jwt',{session:false}), friendsAPI.create);
+router.get('/remove_friendship',passport.authenticate('jwt',{session:false}), friendsAPI.remove);
+
 
 
 module.exports = router;
